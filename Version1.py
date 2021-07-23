a31402769ffd0fabf54624f2c058f5f26e820ea4
Oussama <oussamaelarif@outlook.com>
2021-07-23 14:44:33 +0100

    Minor Adjustments

--- Version1.py
+++ Version1.py
@@ -3,13 +3,12 @@ products_list = ['Latte', 'Cappuccino', 'Tea', 'Espresso', 'Americano']
 
 def main():
     option = int(input('''
-        To Exit press 0
-        To Enter press 1
-        Enter your choice here: '''))
+To Exit press: 0
+To Enter press: 1
+\nEnter your choice here: '''))
 
     if option == 0:
         print("\n Goodbye.")
-        print("")
         exit()
 
     elif option == 1:
@@ -18,43 +17,47 @@ def main():
 
 def product():
     user_input = int(input('''
-    0: To return to the Main Menu
-    1: To view the current drinks menu
-    2: To update existing drinks menu
-    3: To list & replace a drink
-    4: To delete a drink
-    \tSelect from the options above: '''))
+0: To return to the Main Menu
+1: To view the current drinks menu
+2: To update existing drinks menu
+3: To list & replace a drink
+4: To delete a drink
+\nSelect from the options above: '''))
 
     if user_input == 0:
         main()
 
     elif user_input == 1:
-        print("\n The Product List Is : ", products_list)
+        print('\nHere is the beverage menu : ', products_list)
 
     elif user_input == 2:
-        print("\nThe Product List Is : ", products_list)
-        new_product = input("\n Please Enter A New Product Name : ")
+        print('\nHere is the beverage menu : ', products_list)
+        new_product = input('\nPlease add a new beverage to the menu : ')
         products_list.append(new_product)
-        print("\n You Have Created A New Product")
-        print(products_list)
+        print("\nYou've added a beverage to the menu:", products_list)
 
     elif user_input == 3:
-        for value, index in enumerate(products_list):
-            print(value, index)
-        product_index_value = int(
-            input("\n Please Enter The Index value Of The Product You Want To Update : "))
-        new_product_name = input(
-            "\n Please Enter A New Product Name For The Product: ")
-        products_list[product_index_value] = new_product_name
-        print(products_list)
+        print('\nThe beverage selections are: ', '\n')
+        for key, value in enumerate(products_list):
+            print(key, value)
+
+        number_input = int(
+            input('\nChoose a beverage by selecting the number to replace with your desired beverage: '))
+        new_product = input('\nTell us your desired beverage: ')
+
+        products_list[number_input] = new_product
+        print("\nHere's the new updated beverage menu: ", products_list)
 
     elif user_input == 4:
-        print("\n", [list((i, products_list[i]))
-              for i in range(len(products_list))])
-        delete_product_index_value = int(
-            input("\n Please Enter The Index value Of The Product You Want To Delete : "))
-        del products_list[delete_product_index_value]
-        print("\n The New Product List Is :", products_list)
+
+        print('\nLets delete a beverage')
+        for key, value in enumerate(products_list):
+            print(key, value)
+
+        deleted_input = int(
+            input('\nSelected a beverage to be deleted by choosing a number: '))
+        del products_list[deleted_input]
+        print('\nThe new beverage menu is: ', products_list)
 
 
 main()
