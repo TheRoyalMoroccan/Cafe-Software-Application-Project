9b5f3690751d1acfc2f852c841a8770b40e8c7ef
Oussama <oussamaelarif@outlook.com>
2021-07-23 14:45:51 +0100

    Minor Adjusments

--- Version2.py
+++ Version2.py
@@ -40,84 +40,91 @@ def main():
 
 def product():
     user_input = int(input('''
-    0: To Return To The Main Menu
-    1: To View The Current Drinks Menu
-    2: To Update Existing Drinks Menu
-    3: To List & Replace A Drink
-    4: To Delete A Drink
-    \tSelect from the options above: '''))
+0: To return to the Main Menu
+1: To view the current drinks menu
+2: To update existing drinks menu
+3: To list & replace a drink
+4: To delete a drink
+\nSelect from the options above: '''))
 
     if user_input == 0:
         main()
 
     elif user_input == 1:
-        print("\n The Product List Is : ", products_list)
+        print('\nHere is the beverage menu : ', products_list)
 
     elif user_input == 2:
-        print("\nThe Product List Is : ", products_list)
-        new_product = input("\n Please Enter A New Product Name : ")
+        print('\nHere is the beverage menu : ', products_list)
+        new_product = input('\nPlease add a new beverage to the menu : ')
         products_list.append(new_product)
-        print("\n You Have Created A New Product")
-        print(products_list)
+        print("\nYou've added a beverage to the menu:", products_list)
 
     elif user_input == 3:
-        for value, index in enumerate(products_list):
-            print(value, index)
-        product_index_value = int(
-            input("\n Please Enter The Index value Of The Product You Want To Update : "))
-        new_product_name = input(
-            "\n Please Enter A New Product Name For The Product: ")
-        products_list[product_index_value] = new_product_name
-        print(products_list)
+        print('\nThe beverage selections are: ', '\n')
+        for key, value in enumerate(products_list):
+            print(key, value)
+
+        number_input = int(
+            input('\nChoose a beverage by selecting the number to replace with your desired beverage: '))
+        new_product = input('\nTell us your desired beverage: ')
+
+        products_list[number_input] = new_product
+        print("\nHere's the new updated beverage menu: ", products_list)
 
     elif user_input == 4:
-        print("\n", [list((i, products_list[i]))
-                     for i in range(len(products_list))])
-        delete_product_index_value = int(
-            input("\n Please Enter The Index value Of The Product You Want To Delete : "))
-        del products_list[delete_product_index_value]
-        print("\n The New Product List Is :", products_list)
+
+        print('\nLets delete a beverage')
+        for key, value in enumerate(products_list):
+            print(key, value)
+
+        deleted_input = int(
+            input('\nSelected a beverage to be deleted by choosing a number: '))
+        del products_list[deleted_input]
+        print('\nThe new beverage menu is: ', products_list)
 
 
 def courier():
     user_input = int(input('''
-                    0 To Return To The Main Menu
-                    1 To View The Current Couriers List
-                    2 To Create A New Courier
-                    3 To Updates The Existing Couriers List
-                    4 To Delete A Courier
-                    \tSelect from the options above: '''))
+0 To Return To The Main Menu
+1 To View The Current Couriers List
+2 To Create A New Courier
+3 To Updates The Existing Couriers List
+4 To Delete A Courier
+\tSelect from the options above: '''))
 
     if user_input == 0:
         main()
 
     elif user_input == 1:
-        print("\n The Courier List Is : ", couriers_list)
+        print('\nThe courier list is: ', couriers_list)
 
     elif user_input == 2:
-        print("\nThe Courier List Is : ", couriers_list)
-        new_couriers = input("\n Please Enter A New Courier Name : ")
-        couriers_list.append(new_couriers)
-        print("\n You Have Created A New Courier")
-        print(couriers_list)
+        print('\nThe courier list is: ', couriers_list)
+        new_courier = input('\nPlease enter a new courier name: ')
+        couriers_list.append(new_courier)
+        print("\nYou've added a new courier to the list", couriers_list)
 
     elif user_input == 3:
+        print("\nThe couriers list are: ")
         for value, index in enumerate(couriers_list):
             print(value, index)
-        courier_index_value = int(
-            input("\n Please Enter The Index value Of The courier You Want To Update : "))
-        new_courier_name = input(
-            "\n Please Enter A New Name For The Courier: ")
-        couriers_list[courier_index_value] = new_courier_name
-        print(couriers_list)
+
+        number_input = int(input(
+            '\nReplace a courier by selecting the number to associated with the courier:  '))
+        new_courier = input('\nTell us a Name to be added')
+
+        couriers_list[number_input] = new_courier
+        print("\nHere's the new updated courier list: ", couriers_list)
 
     elif user_input == 4:
-        print("\n", [list((i, couriers_list[i]))
-              for i in range(len(couriers_list))])
-        delete_courier_index_value = int(
-            input("\n Please Enter The Index value Of The Courier You Want To Delete : "))
-        del products_list[delete_courier_index_value]
-        print("\n The New Courier List Is :", couriers_list)
+        print('\nLets delete a courier')
+        for key, value in enumerate(couriers_list):
+            print(key, value)
+
+        deleted_input = int(input(
+            '\nSelect a courier to be fired by choosing the number associated with it'))
+        del couriers_list[deleted_input]
+        print('\nThe new courier list is: ', couriers_list)
 
 
 main()
